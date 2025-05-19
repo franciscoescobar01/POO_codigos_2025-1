66c817d97d2ba9862ca81b962f3c2a5389b74b96
Francisco Escobar Jara <francisco.escobar.jara@gmail.com>
2025-05-19 14:41:09 -0400

    Add files via upload

---
+++ untitled54/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+Arr
+        List<Integer> numeros = Arrays.asList(2, 3, 4, 5, 6, 7, 8);
+        numeros.add(2);
+        System.out.println("numeros = " + numeros);
+
+    }
+}
